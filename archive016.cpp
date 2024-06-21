7290d6588acc60b44f68369db6f7d6adb14036ba
Scott Bailey <scott@okbailey.com>
2024-06-21 15:50:14 -0500

    Create archive016.cpp

---
+++ archive016.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main() {
+
+  // NEED TO CONTINUE WORKING ON THIS -- NOT GETTING EXPECTED RESULTS
+  // MOST LIKELY DUE TO DATA TYPE ISSUES
+    
+//   int n = 15;
+//   int exponent = pow(2,n);
+//   string exp_str = to_string(exponent);
+//   cout << &exp_str << endl;
+//   cout << typeid(exp_str).name() << endl;
+  
+//   int digit_sum = 0;
+//   int i = 0;
+//   while (i < exp_str.length()) {
+//     int numeral = int(exp_str[i]);
+//     cout << numeral << endl;
+// //   cout << digit_sum << "\n";
+// //   digit_sum = digit_sum + stoi(exp_str[i]);
+// //   cout << digit_sum << "\n";
+//   i++;   
+//   }
+  
+  char test = "A";
+  cout << int(test) << endl;
+//   string test = "34";  cout << stoi(test);
+//   cout << exponent << "\n";
+//   cout << digit_sum;
+  
+
+
+  return 0;
+}
